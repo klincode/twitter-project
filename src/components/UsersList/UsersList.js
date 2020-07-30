3f9e647a9b43630257e328e767b2b00b464f13ec
Adam Klin <adam.klin@assecobs.pl>
2020-07-30 17:44:51 +0200

    users list ok !!

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -7,91 +7,47 @@ import axios from 'axios';
 const UsersList = () => {
   const [users, setUsersList] = useState([]);
   const [load, setLoad] = useState(true);
-  console.log('=======================1=============');
-  console.log(localStorage.getItem('jwt_token'));
-  // console.log('====================================');
-  const header = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': 'Bearer ' + localStorage.getItem('jwt_token')
-  }
-  console.log('header')
-  console.log(header)
-  useEffect(() => {
-    axios.post(
-      API.endPoints.usersList,
-      header
-    )
+  // console.log('=======================1=============');
+  // console.log(localStorage.getItem('jwt_token'));
+  // // console.log('====================================');
 
+  useEffect(() => {
+    axios({
+      method: 'post',
+      url: API.endPoints.usersList,
+      headers: API.config.headers,
+    })
       .then((res) => {
         console.log(res);
-
-        // setUsersList(res.data);
-        // setLoad(false)
+        setUsersList(res.data);
+        setLoad(false)
       })
       //todo: dodać wyświetlanie błędów
       .then((err) => console.log('blad:' + err))
     // setTimeout(() => setLoginPopupVisible(true), 1000)
   }, [])
 
+
   return (
     <S.Container>
       <S.H2>Warci obserwowania</S.H2>
       <S.Users>
-        <S.User>
-          <S.UserInfo>
-            <S.UserImg><img src={user} alt="" /></S.UserImg>
-            <S.UserLogin>
-              <H3>Adam  ...</H3>
-              <span>@user123</span>
-            </S.UserLogin>
-            <div><Button full inverted>Obserwuj</Button></div>
-          </S.UserInfo>
-        </S.User>
+        {users.map((item, index) => {
+          const { avatar_url, created_at, email, id, updated_at, username } = item;
+          return (
+            <S.User key={index}>
+              <S.UserInfo>
+                <S.UserImg><img src={avatar_url} alt="" /></S.UserImg>
+                <S.UserLogin>
+                  <H3>{username}  ...</H3>
+                  <span>{email}</span>
+                </S.UserLogin>
+                <div><Button full inverted>Obserwuj</Button></div>
+              </S.UserInfo>
+            </S.User>
+          )
+        })}
 
-        <S.User>
-          <S.UserInfo>
-            <S.UserImg><img src={user} alt="" /></S.UserImg>
-            <S.UserLogin>
-              <H3>Adam  ...</H3>
-              <span>@user123</span>
-            </S.UserLogin>
-            <div><Button full inverted>Obserwuj</Button></div>
-          </S.UserInfo>
-        </S.User>
-
-        <S.User>
-          <S.UserInfo>
-            <S.UserImg><img src={user} alt="" /></S.UserImg>
-            <S.UserLogin>
-              <H3>Adam  ...</H3>
-              <span>@user123</span>
-            </S.UserLogin>
-            <div><Button full inverted>Obserwuj</Button></div>
-          </S.UserInfo>
-        </S.User>
-
-        <S.User>
-          <S.UserInfo>
-            <S.UserImg><img src={user} alt="" /></S.UserImg>
-            <S.UserLogin>
-              <H3>Adam  ...</H3>
-              <span>@user123</span>
-            </S.UserLogin>
-            <div><Button full inverted>Obserwuj</Button></div>
-          </S.UserInfo>
-        </S.User>
-
-        <S.User>
-          <S.UserInfo>
-            <S.UserImg><img src={user} alt="" /></S.UserImg>
-            <S.UserLogin>
-              <H3>Adam  ...</H3>
-              <span>@user123</span>
-            </S.UserLogin>
-            <div><Button full inverted>Obserwuj</Button></div>
-          </S.UserInfo>
-        </S.User>
       </S.Users>
     </S.Container>
   );
