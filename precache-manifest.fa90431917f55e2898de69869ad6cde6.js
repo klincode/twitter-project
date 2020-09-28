3ea54813248a5a5e6b4cbb54a1b270788689b8da
Adam Klin <adam.klin@assecobs.pl>
2020-09-28 19:29:06 +0200

    Updates

---
+++ precache-manifest.fa90431917f55e2898de69869ad6cde6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb89eccb8a165db4a403c4ea6dbb22b",
+    "url": "/twitter-project/index.html"
+  },
+  {
+    "revision": "3daba9b16ccaeed7bde6",
+    "url": "/twitter-project/static/js/2.971d6321.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/twitter-project/static/js/2.971d6321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973e8a740585b2d2ae82",
+    "url": "/twitter-project/static/js/main.fdedecd4.chunk.js"
+  },
+  {
+    "revision": "483a61896c2f55ad2964",
+    "url": "/twitter-project/static/js/runtime-main.b8345fd1.js"
+  }
+]);
